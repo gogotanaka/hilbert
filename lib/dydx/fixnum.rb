a4e8ee4b501860d52d5e48d66692c1474a17c7e1
gogotanaka <qlli.illb@gmail.com>
2014-05-05 14:45:00 +0900

    Override Fixnum

---
+++ lib/dydx/fixnum.rb
@@ -0,0 +1,22 @@
+module Dydx
+  Fixnum.class_eval do
+    %w(+ * / ^).each do |operator|
+      define_method(operator) do |g|
+        if g.is_a?(Symbol) ||
+          g.is_a?(Formula) ||
+          g.is_a?(Num) ||
+          g.is_a?(Log) ||
+          g.is_a?(E)
+
+          _(self).send(operator.to_sym, g)
+        else
+          (to_f.send(operator.to_sym, g)).to_i
+        end
+      end
+    end
+
+    def d(sym=:x)
+      _(0)
+    end
+  end
+end
