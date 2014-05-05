71bd30f25ccbaf569c0b85ad10b252a5a918f58c
gogotanaka <qlli.illb@gmail.com>
2014-05-05 14:47:41 +0900

    Add Operator::General

---
+++ lib/dydx/operator/general.rb
@@ -0,0 +1,51 @@
+module Dydx
+  module Operator
+    module General
+      def +(x)
+        if x.is_a?(Num) && x.n ==0
+          self
+        else
+          super(x)
+        end
+      end
+
+      def -(x)
+        if x.is_a?(Num) && x.n ==0
+          self
+        else
+          super(x)
+        end
+      end
+
+      def *(x)
+        if x.is_a?(Num) && x.n ==0
+          x
+        elsif x.is_a?(Num) && x.n ==1
+          self
+        else
+          super(x)
+        end
+      end
+
+      def /(x)
+        if x.is_a?(Num) && x.n ==0
+          raise ZeroDivisionError
+        elsif x.is_a?(Num) && x.n ==1
+          self
+        else
+          super(x)
+        end
+      end
+
+      def ^(x)
+        if x.is_a?(Num) && x.n ==0
+          _(1)
+        elsif x.is_a?(Num) && x.n ==1
+          self
+        else
+          super(x)
+        end
+      end
+    end
+  end
+end
