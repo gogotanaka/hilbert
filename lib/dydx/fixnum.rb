c756e2c434555775202b854cdbcee0569ce955d5
gogotanaka <qlli.illb@gmail.com>
2014-05-06 12:54:32 +0900

    Override Fixnum class and add spec

--- lib/dydx/fixnum.rb
+++ lib/dydx/fixnum.rb
@@ -1,12 +1,13 @@
 module Dydx
   Fixnum.class_eval do
-    %w(+ * / ^).each do |operator|
+    %w(+ - * / ^).each do |operator|
       define_method(operator) do |g|
         if g.is_a?(Symbol) ||
           g.is_a?(Formula) ||
           g.is_a?(Num) ||
           g.is_a?(Log) ||
-          g.is_a?(E)
+          g.is_a?(E) ||
+          %w(/ ^).include?(operator)
 
           _(self).send(operator.to_sym, g)
         else
@@ -14,7 +15,6 @@ module Dydx
         end
       end
     end
-
     def d(sym=:x)
       _(0)
     end
--- spec/lib/fixnum_spec.rb
+++ spec/lib/fixnum_spec.rb
@@ -1,4 +1,52 @@
 require 'spec_helper'
 
 describe Fixnum do
+  context 'With Formula' do
+    let(:formula) { (:x + :y) }
+    it{ expect((0 + formula).to_s).to eq(formula.to_s) }
+    it{ expect((0 - formula).to_s).to eq('( - ( x + y ) )') }
+    it{ expect((0 * formula).to_s).to eq('0') }
+    it{ expect((1 * formula).to_s).to eq(formula.to_s) }
+    it{ expect((0 / formula).to_s).to eq('0') }
+    it{ expect((1 / formula).to_s).to eq('( 1 / ( x + y ) )') }
+    it{ expect((0 ^ formula).to_s).to eq('0') }
+    it{ expect((1 ^ formula).to_s).to eq('1') }
+  end
+
+  context 'With Symbol' do
+    it{ expect(0 + :x).to eq(:x) }
+    it{ expect((0 - :x).to_s).to eq('( - x )') }
+    it{ expect((0 * :x).to_s).to eq('0') }
+    it{ expect(1 * :x).to eq(:x) }
+    it{ expect((0 / :x).to_s).to eq('0') }
+    it{ expect((1 / :x).to_s).to eq('( 1 / x )') }
+    it{ expect((0 ^ :x).to_s).to eq('0') }
+    it{ expect((1 ^ :x).to_s).to eq('1') }
+  end
+
+  context 'With Fixnum' do
+    it{ expect((0 + 3).to_s).to eq('3') }
+    it{ expect((3 + 0).to_s).to eq('3') }
+
+    it{ expect((0 - 3).to_s).to eq('-3') }
+    it{ expect((3 - 0).to_s).to eq('3') }
+
+    it{ expect((0 * 3).to_s).to eq('0') }
+    it{ expect((3 * 0).to_s).to eq('0') }
+
+    it{ expect((1 * 3).to_s).to eq('3') }
+    it{ expect((3 * 1).to_s).to eq('3') }
+
+    it{ expect((0 / 3).to_s).to eq('0') }
+    it{ expect{(3 / 0).to_s}.to raise_error(ZeroDivisionError) }
+
+    it{ expect((1 / 3).to_s).to eq('( 1 / 3 )') }
+    it{ expect((3 / 1).to_s).to eq('3') }
+
+    it{ expect((0 ^ 3).to_s).to eq('0') }
+    it{ expect((3 ^ 0).to_s).to eq('1') }
+
+    it{ expect((1 ^ 3).to_s).to eq('1') }
+    it{ expect((3 ^ 1).to_s).to eq('3') }
+  end
 end
