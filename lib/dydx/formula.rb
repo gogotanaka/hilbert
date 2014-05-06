62af18a3e8edd8134afe64cedc4ecadec4fb6e8e
gogotanaka <qlli.illb@gmail.com>
2014-05-06 12:53:08 +0900

    Add Formula class

---
+++ lib/dydx/formula.rb
@@ -0,0 +1,42 @@
+require "dydx/helper"
+module Dydx
+  class Formula
+    include Operator::Base
+    include Operator::General
+    include Operator::Interface
+    include Helper
+    attr_accessor :f, :g, :operator
+
+    def initialize(f, g, operator)
+      @f, @g, @operator = f, g, operator
+    end
+
+    def d(sym=:x)
+      case @operator
+      when :+
+        f.d(sym) + g.d(sym)
+      when :-
+        f.d(sym) - g.d(sym)
+      when :*
+        (f.d(sym) * g) + (f * g.d(sym))
+      when :/
+        ((f.d(sym) * g) - (f * g.d(sym)))/(g ^ _(2))
+      when :^
+        # TODO:
+        if f == sym
+          g * (f ^ (g - 1))
+        else
+          self * (g * log(f)).d(sym)
+        end
+      end
+    end
+
+    def to_s
+      if is_a?(Formula) && subtraction? && f.is_0?
+        "( - #{g.to_s} )"
+      else
+        "( #{f.to_s} #{@operator} #{g.to_s} )"
+      end
+    end
+  end
+end
---
+++ lib/dydx/helper.rb
@@ -0,0 +1,21 @@
+module Dydx
+  module Helper
+    def is_0?
+      is_a?(Num) && n == 0
+    end
+
+    def is_1?
+      is_a?(Num) && n == 1
+    end
+
+    {
+      addition:       :+,
+      subtraction:    :-,
+      multiplication: :*,
+      division:       :/,
+      exponentiation: :^
+    }.each do |operator_name, operator|
+      define_method("#{operator_name}?") { @operator == operator }
+    end
+  end
+end
