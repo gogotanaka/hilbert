08983b5cababc9b75ed443592c951eca44c6e624
matatabi step <talu@consultant.com>
2014-05-19 22:01:14 +0900

    Add specs

--- spec/lib/algebra/formula_spec.rb
+++ spec/lib/algebra/formula_spec.rb
@@ -9,13 +9,13 @@ describe Dydx::Algebra::Formula do
   describe 'Calculate' do
     context 'With Fixnum' do
       let(:formula) { (:x + :y) }
-      it{ expect(formula + 0).to eq(formula + 0) }
-      it{ expect(formula - 0).to eq(formula - 0) }
-      it{ expect((formula * 0).to_s).to eq('0') }
-      it{ expect((formula * 1).to_s).to eq(formula.to_s) }
+      it{ expect(formula + 0).to eq(formula) }
+      it{ expect(formula - 0).to eq(formula) }
+      it{ expect(formula * 0).to eq(0) }
+      it{ expect(formula * 1).to eq(formula) }
       it{ expect{(formula / 0).to_s}.to raise_error(ZeroDivisionError) }
       it{ expect(formula / 1).to eq(formula) }
-      it{ expect((formula ^ 0).to_s).to eq('1') }
+      it{ expect(formula ^ 0).to eq(1) }
     end
   end
 
@@ -29,25 +29,25 @@ describe Dydx::Algebra::Formula do
   end
 
   describe '#differentiate' do
-    it{ expect(addition.d(:x).to_s).to eq('1') }
-    it{ expect(addition.d(:y).to_s).to eq('1') }
-    it{ expect(addition.d(:z).to_s).to eq('0') }
+    it{ expect(addition.d(:x)).to eq(1) }
+    it{ expect(addition.d(:y)).to eq(1) }
+    it{ expect(addition.d(:z)).to eq(0) }
 
-    it{ expect(subtraction.d(:x).to_s).to eq('1') }
-    it{ expect(subtraction.d(:y).to_s).to eq('( - 1 )') }
-    it{ expect(subtraction.d(:z).to_s).to eq('0') }
+    it{ expect(subtraction.d(:x)).to eq(1) }
+    it{ expect(subtraction.d(:y)).to eq('( - 1 )') }
+    it{ expect(subtraction.d(:z)).to eq(0) }
 
     it{ expect(multiplication.d(:x)).to eq(:y) }
     it{ expect(multiplication.d(:y)).to eq(:x) }
-    it{ expect(multiplication.d(:z).to_s).to eq('0') }
+    it{ expect(multiplication.d(:z)).to eq(0) }
 
-    it{ expect(division.d(:x).to_s).to eq("( 1 / y )") }
-    it{ expect(division.d(:y).to_s).to eq('( - ( x / ( y ^ 2 ) ) )') }
-    it{ expect(division.d(:z).to_s).to eq('0') }
+    it{ expect(division.d(:x)).to eq(1/:y) }
+    it{ expect(division.d(:y)).to eq('( - ( x / ( y ^ 2 ) ) )') }
+    it{ expect(division.d(:z)).to eq(0) }
 
     it{ expect(exponentiation.d(:x).to_s).to eq('( y * ( x ^ ( y - 1 ) ) )') }
-    it{ expect(exponentiation.d(:y).to_s).to eq('( ( x ^ y ) * log( x ) )') }
-    it{ expect(exponentiation.d(:z).to_s).to eq('0') }
+    it{ expect(exponentiation.d(:y)).to eq((:x ^ :y) * log(:x)) }
+    it{ expect(exponentiation.d(:z)).to eq(0) }
   end
 
   describe '#include?' do
