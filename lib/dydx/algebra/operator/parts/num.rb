ad1273c82a2e20c7fd0246cb8c5dcdd97f694df1
gogotanaka <qlli.illb@gmail.com>
2014-05-20 11:28:31 +0900

    Refactor Operator::Num

--- lib/dydx/algebra/operator/parts/num.rb
+++ lib/dydx/algebra/operator/parts/num.rb
@@ -3,39 +3,29 @@ module Dydx
     module Operator
       module Parts
         module Num
-          def +(x)
-            if n == 0
-              x
-            elsif x.is_a?(Num)
-              _(n + x.n)
-            elsif x.subtrahend? && x.x.is_a?(Num)
-              _(n - x.x.n)
-            else
-              super(x)
-            end
-          end
-
-          def *(x)
-            if n == 0
-              self
-            elsif n == 1
-              x
-            elsif x.is_a?(Num)
-              _(n * x.n)
-            elsif x.divisor? && x.x.is_a?(Num)
-              _(n / x.x.n)
-            else
-              super(x)
-            end
-          end
-
-          def ^(x)
-            if (n == 0) || (n == 1)
-              self
-            elsif x.is_a?(Num)
-              _(n ^ x.n)
-            else
-              super(x)
+          %w(+ * ^).map(&:to_sym).each do |operator|
+            define_method(operator) do |x|
+              if is_0?
+                case operator
+                when :+ then x
+                when :* then e0
+                when :^ then e0
+                end
+              elsif is_1?
+                case operator
+                when :+ then super(x)
+                when :* then x
+                when :^ then e1
+                end
+              elsif x.is_a?(Num)
+                _(n.send(operator, x.n))
+              elsif operator == :+ && x.subtrahend? && x.x.is_a?(Num)
+                _(n - x.x.n)
+              elsif operator == :* && x.divisor? && x.x.is_a?(Num)
+                _(n / x.x.n)
+              else
+                super(x)
+              end
             end
           end
         end
