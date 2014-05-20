c7093916191246641d7373ff1aba617d5ee792bf
gogotanaka <qlli.illb@gmail.com>
2014-05-20 11:37:44 +0900

    Refactor Operator::Inverse

--- lib/dydx/algebra/operator/parts/inverse.rb
+++ lib/dydx/algebra/operator/parts/inverse.rb
@@ -3,30 +3,20 @@ module Dydx
     module Operator
       module Parts
         module Inverse
-          def +(x)
-            if self.x == x && operator == :+
-              e0
-            elsif !x.is_a?(Inverse)
-              x + self
-            else
-              super(x)
-            end
-          end
-
-          def *(x)
-            if self.x == x && operator == :*
-              e1
-            else
-              super(x)
-            end
-          end
-
-          def ^(x)
-            case operator
-            when :+
-              super(x)
-            when :*
-              inverse(self.x ^ x, :*)
+          %w(+ * ^).map(&:to_sym).each do |operator|
+            define_method(operator) do |x|
+              if inverse?(x, operator)
+                case operator
+                when :+ then e0
+                when :* then e1
+                end
+              elsif !x.is_a?(Inverse) && operator == :+
+                x + self
+              elsif self.operator == :* && operator == :^
+                inverse(self.x ^ x, :*)
+              else
+                super(x)
+              end
             end
           end
         end
