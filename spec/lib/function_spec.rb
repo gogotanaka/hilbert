575dcaaf7f73e5fc3da12b518084f862e21021a1
gogotanaka <qlli.illb@gmail.com>
2014-05-24 21:39:26 +0900

    Add examples to function.spec

--- spec/lib/function_spec.rb
+++ spec/lib/function_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
 describe Dydx:Function do
+  # TODO: refactor
   it{ expect(f(x, y)).to eq(f(x, y)) }
+  it{ expect(f(x, y)).to eq($f) }
   it{ expect{f(x, y, z)}.to raise_error(ArgumentError) }
   it{ expect{f(x)}.to raise_error(ArgumentError) }
   it{ expect(f(x, y) <= x * y ).to eq(f(x, y)) }
+  it{ expect(f(x, y)).to eq(f(x, y)) }
+  it{ expect(f(x, y)).to eq($f) }
   it{ expect(f(a, b)).to eq(a * b) }
   it{ expect(f(2, 3)).to eq(6) }
   it{ expect(f(a + b, c)).to eq((a + b) * c) }
