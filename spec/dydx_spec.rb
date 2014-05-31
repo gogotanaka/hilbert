a02a761bc32e3fcc413de5478b969361a6e0a1be
gogotanaka <qlli.illb@gmail.com>
2014-06-01 04:46:36 +0900

    Improve diff spec

--- spec/dydx_spec.rb
+++ spec/dydx_spec.rb
@@ -4,36 +4,4 @@ describe Dydx do
   it 'has a version number' do
     expect(Dydx::VERSION).not_to be nil
   end
-
-  context 'ex1' do
-    $a = (:x ^ :n)
-    let(:d1){ da/dx }
-    let(:d2){ d/dx($a) }
-    it{ expect(d1.to_s).to eq('( n * ( x ^ ( n - 1 ) ) )') }
-    it{ expect(d2.to_s).to eq('( n * ( x ^ ( n - 1 ) ) )') }
-  end
-
-  context 'ex2' do
-    $b = (:x ^ (:x * 2))
-    let(:d1){ db/dx }
-    let(:d2){ d/dx($b) }
-    it{ expect(d1.to_s).to eq('( ( 2 * x ) * ( x ^ ( ( 2 * x ) - 1 ) ) )') }
-    it{ expect(d2.to_s).to eq('( ( 2 * x ) * ( x ^ ( ( 2 * x ) - 1 ) ) )') }
-  end
-
-  context 'ex3' do
-    $c = (:t ^ 2) / 2
-    let(:d1){ dc/dt }
-    let(:d2){ d/dt($c) }
-    it{ expect(d1.to_s).to eq('t') }
-    it{ expect(d2.to_s).to eq('t') }
-  end
-
-  context 'ex4' do
-    $i = 2 * (e ^ (2 * :z))
-    let(:d1){ di/dz }
-    let(:d2){ d/dz($i) }
-    it{ expect(d1.to_s).to eq('( 4 * ( e ^ ( 2 * z ) ) )') }
-    it{ expect(d2.to_s).to eq('( 4 * ( e ^ ( 2 * z ) ) )') }
-  end
 end
--- spec/lib/delta_spec.rb
+++ spec/lib/delta_spec.rb
@@ -8,4 +8,30 @@ describe Dydx:Delta do
   it{ expect(dx(y).var).to eq(:x) }
   it{ expect(dx(y).function).to eq(:y) }
   it{ expect{dxy}.to raise_error(NameError) }
+
+  before(:each) do
+    $y = example
+  end
+
+  context 'ex1' do
+    let(:example) { x ^ n }
+    it { expect(dy/dx).to eq( n * ( x ^ ( n - 1 ) ) ) }
+    it { expect(d/dx($y)).to eq( n * ( x ^ ( n - 1 ) ) ) }
+  end
+
+  context 'ex2' do
+    let(:example) { x ^ (x * 2) }
+    it{ expect(dy/dx).to eq(( 2 * x ) * ( x ^ ( ( 2 * x ) - 1 ) )) }
+    it{ expect(d/dx($y)).to eq(( 2 * x ) * ( x ^ ( ( 2 * x ) - 1 ) )) }
+  end
+
+  context 'ex3' do
+    let(:example) { (t ^ 2) / 2 }
+    it{ expect(dy/dt).to eq(t) }
+  end
+
+  context 'ex4' do
+    let(:example) { 2 * (e ^ (2 * z)) }
+    it{ expect(dy/dz).to eq(4 * ( e ^ ( 2 * z ) )) }
+  end
 end
