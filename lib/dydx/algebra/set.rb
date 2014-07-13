eac1bc84cf186d3496cea7d031917ca00a31fbb3
gogotanaka <qlli.illb@gmail.com>
2014-07-13 16:13:55 -0700

    Refactor sin and cos

--- lib/dydx/algebra/set.rb
+++ lib/dydx/algebra/set.rb
@@ -276,44 +276,40 @@ module Dydx
         end
       end
 
-      # TODO: We should hundle Rational num
+      # TODO: We should negative num
       def sin(x)
-        if x.multiple_of?(pi)
-          if (x / pi).is_a?(Num)
-            case (x / pi) % 2
-            when 0 then 0
-            when 1 then 0
-            else
-              Sin.new(x)
-            end
-          elsif (x / pi) == (_(1) / _(2))
-            1
-          elsif (x / pi) == (_(3) / _(2))
-            -1
-          else
-            Sin.new(x)
-          end
-        else
-          Sin.new(x)
+        return Sin.new(x) unless x.multiple_of?(pi) && (x / pi).num?
+
+        radn = (x / pi)
+        loop do
+          break if radn < 2
+          radn -= 2
+        end
+
+        case radn
+        when 0        then 0
+        when _(1) / 2 then 1
+        when 1        then 0
+        when _(3) / 2 then -1
+        else               Sin.new(x)
         end
       end
 
       def cos(x)
-        if x.multiple_of?(pi)
-          if (x / pi).is_a?(Num)
-            case (x / pi) % 2
-            when 0 then 1
-            when 1 then -1
-            else
-              Cos.new(x)
-            end
-          elsif [(_(1) / _(2)), (_(3) / _(2))].include? (x / pi)
-            0
-          else
-            Cos.new(x)
-          end
-        else
-          Cos.new(x)
+        return Cos.new(x) unless x.multiple_of?(pi) && (x / pi).num?
+
+        radn = (x / pi)
+        loop do
+          break if radn < 2
+          radn -= 2
+        end
+
+        case radn
+        when 0        then 1
+        when _(1) / 2 then 0
+        when 1        then -1
+        when _(3) / 2 then 0
+        else               Sin.new(x)
         end
       end
 
