5eea3ab4fa556b13a4decad2ffbbbb753b8cefee
gogotanaka <qlli.illb@gmail.com>
2014-08-08 20:48:56 +0900

    Improve exec.rb

--- lib/q/exec.rb
+++ lib/q/exec.rb
@@ -2,7 +2,25 @@ module Q
   module Exec
     class Compiler
       def initialize(args)
-        filename = args.first
+        @args = args
+      end
+
+      def parse!
+        parse
+      rescue Exception => e
+        raise e if @options[:trace] || e.is_a?(SystemExit)
+
+        print "#{e.class}: " unless e.class == RuntimeError
+        puts "#{e.message}"
+        puts "  Use --trace for backtrace."
+        exit 1
+      ensure
+        exit 0
+      end
+
+      private def parse
+        raise '#{@args[0]} is unsupported option' unless @args[0] == '-q'
+        filename = @args[1]
         file = open_file(filename)
         string = read_file(file)
         print(Kconv.tosjis(Q.compile(string)), '\n')
