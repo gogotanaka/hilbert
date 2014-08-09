02cbe0b475ccad8130117fbdee3a1665546e10a2
gogotanaka <qlli.illb@gmail.com>
2014-08-09 15:30:30 -0700

    Add examples to List

---
+++ spec/list_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe QLang do
+  describe 'List' do
+    it do
+      expect(
+        Q.compile('{name: "Gogotanaka", age:  21, birth: (1992 8 10) }')
+      ).to eq(
+        "list(name=\"Gogotanaka\", age=21, birth=c(1992, 8, 10))"
+      )
+
+      expect(
+        Q.compile('{key1: 234234, key2: 387342 }')
+      ).to eq(
+        "list(key1=234234, key2=387342)"
+      )
+
+      expect(
+        Q.compile('{key1:234234,key2:387342,key3:38733242}')
+      ).to eq(
+        "list(key1=234234, key2=387342, key3=38733242)"
+      )
+
+      expect(
+        Q.compile('{key1:(1 3 2; 8 2 3),key2:387342,key3:38733242}')
+      ).to eq(
+        "list(key1=matrix(c(1, 3, 2, 8, 2, 3), 2, 3, byrow = TRUE), key2=387342, key3=38733242)"
+      )
+    end
+  end
+end
