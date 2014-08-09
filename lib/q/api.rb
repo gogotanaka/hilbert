d900db8734c2623d5574e7f6ab50031fec1847d3
gogotanaka <qlli.illb@gmail.com>
2014-08-09 12:55:20 +0900

    Implement func api

--- lib/q/api.rb
+++ lib/q/api.rb
@@ -1,6 +1,7 @@
 require 'q/api/matrix_api'
 require 'q/api/vector_api'
 require 'q/api/list_api'
+require 'q/api/func_api'
 
 module Q
   module Api
---
+++ lib/q/api/func_api.rb
@@ -0,0 +1,10 @@
+module Q
+  module Api
+    module FuncApi
+      def execute(func_name, args, contents)
+        "#{func_name} <- function(#{ args.join(' ,') }) #{contents}"
+      end
+      module_function :execute
+    end
+  end
+end
