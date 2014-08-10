a3180290a14368a956c98683c3682df08554dbc6
gogotanaka <qlli.illb@gmail.com>
2014-08-09 21:04:08 -0700

    Add a example

--- spec/iq_spec.rb
+++ spec/iq_spec.rb
@@ -4,6 +4,7 @@ describe Qlang do
   describe Iq do
     it do
       expect(Iq.execute('(1 2 3; 4 5 6)')).to eq('(1 2 3; 4 5 6)')
+      expect(Iq.execute('(1 2 3; 4 5 6) + (1 2 3; 4 5 6)')).to eq('(2 4 6; 8 10 12)')
       expect(Iq.execute('(1 2 3)')).to eq('(1 2 3)')
       expect(Iq.execute('d/dx(sin(x))')).to eq(cos(x))
       expect(Iq.execute('d/dx(log(x))')).to eq(1/x)
