102f41bcbdfa16c600b7c4d2fb673495b9e8c080
gogotanaka <qlli.illb@gmail.com>
2014-08-09 19:48:17 -0700

    Add example for dydx

--- spec/q_lang_spec.rb
+++ spec/q_lang_spec.rb
@@ -12,6 +12,24 @@ describe Qlang do
   describe Dydx do
     it 'check some example' do
       expect(d/dx(sin(x))).to eq cos(x)
+
+      expect(
+        Q.to_ruby.compile('d/dx(sin(x))')
+      ).to eq(
+        "d/dx(sin (x))"
+      )
+
+      expect(
+        Q.to_ruby.compile('d/dx(log(x))')
+      ).to eq(
+        "d/dx(log (x))"
+      )
+
+      expect(
+        Q.to_ruby.compile('f(x, y) = x + y')
+      ).to eq(
+        "f(x ,y) <= x + y"
+      )
     end
   end
 end
