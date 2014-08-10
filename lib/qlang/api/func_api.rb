d133649df27c466fd6b789cfea3ac3d0b5efee9c
gogotanaka <qlli.illb@gmail.com>
2014-08-09 19:47:41 -0700

    Extend function into Ruby

--- lib/qlang/api/func_api.rb
+++ lib/qlang/api/func_api.rb
@@ -2,7 +2,13 @@ module Qlang
   module Api
     module FuncApi
       def execute(func_name, args, contents)
-        "#{func_name} <- function(#{ args.join(' ,') }) #{contents}"
+        case $type
+        when :R
+          "#{func_name} <- function(#{ args.join(' ,') }) #{contents}"
+        when :Ruby
+          "#{func_name}(#{ args.join(' ,') }) <= #{contents}"
+        end
+
       end
       module_function :execute
     end
