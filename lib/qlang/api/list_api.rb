e20b65dbe6418cde6801834bdcf44aecb5ba1bc3
gogotanaka <qlli.illb@gmail.com>
2014-08-14 23:51:19 -0700

    Error handle for compiling into Ruby

--- lib/qlang/api/list_api.rb
+++ lib/qlang/api/list_api.rb
@@ -2,8 +2,14 @@ module Qlang
   module Api
     module ListApi
       def execute(arys)
-        combineds_by_equal = arys.map { |ary| "#{ary[0]}=#{ary[1]}" }.join(', ')
-        "list(#{combineds_by_equal})"
+        case $type
+        when :R
+          combineds_by_equal = arys.map { |ary| "#{ary[0]}=#{ary[1]}" }.join(', ')
+          "list(#{combineds_by_equal})"
+        when :Ruby
+          fail 'List is not implemented for Ruby'
+        end
+
       end
       module_function :execute
     end
