58db84bcb11b7a52e723531b82a4f2e75f12d4dc
gogotanaka <mail@tanakakazuki.com>
2014-10-18 13:48:03 -0700

    Refactoring ope

--- lib/qlang/lexer/tokens.rb
+++ lib/qlang/lexer/tokens.rb
@@ -8,16 +8,25 @@ module Qlang
       PI = /pi/
       NUM = /(#{INT}|#{FLO}|#{E}|#{PI})/
 
+      # FUNCTION
       EMBEDDED_FUNC = /(sin|cos|tan|log)/
       USER_FUNC = /[a-zA-Z]/
-
       FUNCV = /(#{EMBEDDED_FUNC}|#{USER_FUNC})/
 
+      # VARIABLE
       VAR = /([a-d]|[f-z])/
       VAR_MUL2 = /(?!pi)#{VAR}{2}/
       VAR_MUL3 = /(?!#{EMBEDDED_FUNC})#{VAR}{3}/
       # FIX:
       VAR_MUL  = /(?!#{EMBEDDED_FUNC})#{VAR_MUL2}/
+
+      # OPE
+      PLS = /\+/
+      SUB = /-/
+      MUL = /\*/
+      DIV = /\//
+      EXP = /(\*\*|\^)/
+      OPE = /(#{PLS}|#{SUB}|#{MUL}|#{DIV}|#{EXP})/
 
       VARNUM = /(#{NUM}|#{VAR})/
       ANYSP = ' *'
@@ -50,7 +59,6 @@ module Qlang
       NUMS_BY_SP = Tmp.string_out(NUM, SP)
 
       # THIRD TOKEN
-
       FUNCCN =  Tmp.func_call(NUMS_BY_CMA)
       FUNCCV = Tmp.func_call(VARS_BY_CMA)
       FUNCCVN =  Tmp.func_call(VARNUMS_BY_CMA)
@@ -58,14 +66,7 @@ module Qlang
       NUMS_BY_SP_BY_SCLN = Tmp.string_out(NUMS_BY_SP, SCLN)
 
 
-      # FORM
-      PLS = '\+'
-      SUB = '-'
-      MUL = '\*'
-      DIV = '\/'
-      EXP = '(\*\*|\^)'
-
-      FORMULA = "(#{PLS}|#{SUB}|#{MUL}|#{DIV}|#{EXP}|#{VARNUM}|#{LPRN}|#{RPRN}| )+"
+      FORMULA = "(#{VARNUM}|#{LPRN}|#{RPRN}| )+"
     end
   end
 end
