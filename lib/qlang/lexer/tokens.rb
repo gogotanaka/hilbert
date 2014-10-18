d09f3deb88cc674412e2649fcda004c23d27f803
gogotanaka <mail@tanakakazuki.com>
2014-10-18 12:29:59 -0700

    Refactor tokens

--- lib/qlang/lexer/tokens.rb
+++ lib/qlang/lexer/tokens.rb
@@ -2,44 +2,58 @@ module Qlang
   module Lexer
     module Tokens
       # FIRST TOKEN
-      NUM = '[0-9]+'
+      INT = /[0-9]+/
+      FLO = /[0-9]+\.[0-9]+/
+      NUM = "(#{INT}|#{FLO})"
       VAR = '[a-z]'
       FUNCV = '[a-zA-Z]'
-      VARNUM = '([0-9]+|[a-z])'
+      VARNUM = "(#{NUM}|#{VAR})"
       ANYSP = ' *'
       ANYSTR = '.+'
       NONL = '[^\r\n]'
-      LPRN = '\('
-      RPRN = '\)'
-      LBRC = '\{'
-      RBRC = '\}'
-      CLN = '\:'
-      SCLN = ';'
-      CMA = '\,'
-      SP = ' '
+      LPRN = /\(/
+      RPRN = /\)/
+      LBRC = /\{/
+      RBRC = /\}/
+      CLN = /\:/
+      SCLN = /;/
+      CMA = /\,/
+      SP = / /
 
       # SECOND TOKEN
-      class ::String
-        def line_by(char)
-          "#{ANYSP}#{self}(#{ANYSP}#{char}#{ANYSP}#{self})*#{ANYSP}"
+      class Tmp
+        def self.string_out(str, partition)
+        "#{ANYSP}#{str}(#{ANYSP}#{partition}#{ANYSP}#{str})*#{ANYSP}"
+        end
+
+        def self.func_call(args)
+          "#{FUNCV}#{LPRN}#{ANYSP}#{args}#{ANYSP}#{RPRN}"
         end
       end
-      NUMS_BY_CMA = NUM.line_by(CMA)
-      VARS_BY_CMA = VAR.line_by(CMA)
-      VARNUMS_BY_CMA = VARNUM.line_by(CMA)
-      NUMS_BY_SP = NUM.line_by(SP)
+
+
+      NUMS_BY_CMA = Tmp.string_out(NUM, CMA)
+      VARS_BY_CMA = Tmp.string_out(VAR, CMA)
+      VARNUMS_BY_CMA = Tmp.string_out(VARNUM, CMA)
+      NUMS_BY_SP = Tmp.string_out(NUM, SP)
 
       # THIRD TOKEN
-      class ::String
-        def func_call
-          "#{FUNCV}#{LPRN}#{ANYSP}#{self}#{ANYSP}#{RPRN}"
-        end
-      end
-      FUNCCN =  NUMS_BY_CMA.func_call
-      FUNCCV = VARS_BY_CMA.func_call
-      FUNCCVN =  VARNUMS_BY_CMA.func_call
 
-      NUMS_BY_SP_BY_SCLN = NUMS_BY_SP.line_by(SCLN)
+      FUNCCN =  Tmp.func_call(NUMS_BY_CMA)
+      FUNCCV = Tmp.func_call(VARS_BY_CMA)
+      FUNCCVN =  Tmp.func_call(VARNUMS_BY_CMA)
+
+      NUMS_BY_SP_BY_SCLN = Tmp.string_out(NUMS_BY_SP, SCLN)
+
+
+      # FORM
+      PLS = '\+'
+      SUB = '-'
+      MUL = '\*'
+      DIV = '\/'
+      EXP = '(\*\*|\^)'
+
+      FORMULA = "(#{PLS}|#{SUB}|#{MUL}|#{DIV}|#{EXP}|#{VARNUM}|#{LPRN}|#{RPRN}| )+"
     end
   end
 end
