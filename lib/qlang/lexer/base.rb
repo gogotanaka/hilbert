447095b26f7e13bec470c8c65baaec6888c4b500
gogotanaka <mail@tanakakazuki.com>
2014-10-22 02:38:47 -0700

    Refactoring Parser base class

--- lib/qlang/lexer/base.rb
+++ lib/qlang/lexer/base.rb
@@ -7,12 +7,12 @@ module Qlang
       attr_accessor :lexeds
       include Tokens
       class << self
-        attr_reader :token_hash
+        attr_reader :token_rule_hash
 
         def rule(pattern, &token)
           token ||= proc { :NULL }
-          @token_hash ||= {}
-          @token_hash[token.call] = pattern
+          @token_rule_hash ||= {}
+          @token_rule_hash[token.call] = pattern
         end
       end
 
@@ -20,13 +20,24 @@ module Qlang
         ss = StringScanner.new(str)
         @lexeds = []
         until ss.eos?
-          self.class.token_hash.each do |token, patter|
-            if ss.scan(patter)
-              (@lexeds << {token => ss[0], els: [ss[1],ss[2], ss[3]].compact }) unless token == :NULL
-              break
-            end
+          scan_rslt, ss = scan(ss)
+          if scan_rslt
+            @lexeds << scan_rslt unless scan_rslt == :NULL
+          else
+            fail "I'm so sorry, something wrong. Please feel free to report this."
           end
         end
+      end
+
+      def scan(ss)
+        scan_rslt = nil
+        token_rule_hash.each do |token, patter|
+          if ss.scan(patter)
+            scan_rslt = (token == :NULL) ? :NULL : {token => ss[0], els: [ss[1],ss[2], ss[3]].compact }
+            break
+          end
+        end
+        [scan_rslt, ss]
       end
 
       # Accessor
@@ -38,6 +49,10 @@ module Qlang
 
       def token_str
         @lexeds.map.with_index { |lexed, i| ":#{lexed.keys.first}#{i}" }.join
+      end
+
+      def token_rule_hash
+        self.class.token_rule_hash
       end
 
       ## POST(with side effect, without idempotence.)
@@ -60,10 +75,6 @@ module Qlang
       # Legacy Accessor
       def values
         @lexeds.map { |lexed| lexed.values.first }
-      end
-
-      def [](index)
-        @lexeds[index]
       end
 
       private
