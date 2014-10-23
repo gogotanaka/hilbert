cc013bc108b9b7c5f8e6f9dcef170504a8189cf5
gogotanaka <mail@tanakakazuki.com>
2014-10-22 19:06:10 -0700

    Init minitest experimentally

---
+++ spec/minitests/q_lang.rb
@@ -0,0 +1,26 @@
+require 'test/unit'
+require './lib/qlang'
+
+class TestVector < Test::Unit::TestCase
+  def setup
+
+  end
+
+  def assert_to_ruby(input, output)
+    assert_equal(Q.to_ruby.compile(input), output)
+  end
+
+  def test_basis
+    assert_not_equal(Qlang::VERSION, nil)
+    assert_equal(Qlang, Q)
+  end
+
+  def test_demo_code
+    assert_to_ruby('d/dx(sin(x))', 'd/dx(sin(x))')
+    assert_to_ruby('d/dx(log(x))', 'd/dx(log(x))')
+    assert_to_ruby('f(x, y) = x + y', 'f(x, y) <= x + y')
+    assert_equal(Matrix[[1, 2, 3], [4, 5, 6]].to_q, '(1 2 3; 4 5 6)')
+    assert_equal(Vector[1, 2, 3].to_q, '(1 2 3)')
+  end
+
+end
