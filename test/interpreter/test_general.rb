a3f81a9a3ef94c2404a035d494eff9ca3ead4f46
gogotanaka <mail@tanakakazuki.com>
2014-10-26 10:52:51 -0700

    Init test_general.rb

---
+++ test/interpreter/test_general.rb
@@ -0,0 +1,13 @@
+require 'minitest_helper'
+
+class TestGeneral < TestInterpreterBase
+  def setup
+
+  end
+
+  def test_general
+    assert_iq_equal('2x', '2x')
+    assert_iq_equal('x + x', '2x')
+    assert_iq_equal('x * y', 'xy')
+  end
+end
