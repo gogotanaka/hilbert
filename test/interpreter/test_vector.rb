efc0e7e08829063fe64ea45589ba594941f8072e
gogotanaka <mail@tanakakazuki.com>
2014-10-26 10:52:12 -0700

    test/interpreter/test_vector.rb

--- test/interpreter/test_vector.rb
+++ test/interpreter/test_vector.rb
@@ -1,1 +1,29 @@
 require 'minitest_helper'
+
+class TestVector < TestInterpreterBase
+  def setup
+
+  end
+
+  def test_integer
+    assert_iq_equal(
+      '(1 2 3)',
+      '(1 2 3)'
+    )
+
+    assert_iq_equal(
+      '(1 2 3) + (1 2 3)',
+      '(2 4 6)'
+    )
+
+    assert_iq_equal(
+      '(1  2  3 )  +  ( 1 2 3 )',
+      '(2 4 6)'
+    )
+
+    assert_iq_equal(
+      '(1 2 3) - (1 2 3) - (1 2 3)',
+      '(-1 -2 -3)'
+    )
+  end
+end
