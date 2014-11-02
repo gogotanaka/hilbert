a56bec82b747502ac68bb62c0d5756a1a539e36f
Paulo Gonzaga <paulo.gonzaga@gmail.com>
2014-11-02 11:20:43 -0500

    Fix spacing

--- lib/qlang/parser/matrix_parser.rb
+++ lib/qlang/parser/matrix_parser.rb
@@ -2,7 +2,7 @@ module Qlang
   module Parser
     module MatrixParser
       include Base
-      def execute(els, opts={trans: false})
+      def execute(els, opts = { trans: false })
         trans = opts[:trans]
         rows = els.first.split(/ *(?:;|\n) */).map(&:split_by_sp)
         rows.all? { |row| row.count == rows.first.count }
