1750822d0577e69ab3784d635402cf87474e6f9f
gogotanaka <mail@tanakakazuki.com>
2014-11-16 13:39:08 -0800

    Update test general

--- test/interpreter/test_general.rb
+++ test/interpreter/test_general.rb
@@ -4,9 +4,17 @@ class TestGeneral < TestInterpreterBase
   def setup
   end
 
+  # TODO: opposite
+  def assert_iq_equal(output, input)
+    assert_equal(output, Hilbert::Iq.execute(input))
+  end
+
   def test_general
     assert_iq_equal('2x', '2x')
-    assert_iq_equal('x + x', '2x')
-    assert_iq_equal('x * y', 'xy')
+    assert_iq_equal('2x', 'x + x')
+    # assert_iq_equal('xy', 'xy')
+    assert_iq_equal('x ^ y', 'x ^ y')
+    assert_iq_equal('2sin( x )', 'sin(x) + sin(x)')
+    assert_iq_equal('log( x ) ^ 2', 'log(x) * log(x)')
   end
 end
