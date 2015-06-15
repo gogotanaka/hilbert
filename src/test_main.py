52615e9569fbdcef73db5aa38e410e379154c3df
gogotanaka <mail@tanakakazuki.com>
2015-06-14 23:30:29 -0700

    Refactoring tests

--- src/test_main.py
+++ src/test_main.py
@@ -1,137 +1,89 @@
 import unittest
 import h_parser
-
-try:
-    import StringIO
-except ImportError:
-    import io as StringIO
-
+import io
 import sys
 
-def check(test_obj, expected):
-    test_obj.assertEqual(sys.stdout.getvalue().replace("\n", ""), expected)
-
-def h_eval(output, input, test_obj):
+def hAssertEqual(test_obj, output, input):
     h_parser.parser.parse(input)
-    check(test_obj, output)
+    test_obj.assertEqual(sys.stdout.getvalue().replace("\n", ""), output)
+    sys.stdout = sys.__stdout__
+    sys.stdout = io.StringIO()
+
 
 class TestMainMethods(unittest.TestCase):
     def setUp(self):
-        sys.stdout = StringIO.StringIO()
+        sys.stdout = io.StringIO()
 
-    def test_ex1(self):
-        print('d')
-        check(self, 'd')
+    def test_basis(self):
+        hAssertEqual(self, 'a', 'a')
+        hAssertEqual(self, 'b', 'b')
+        hAssertEqual(self, '2', '2')
+        hAssertEqual(self, '3', '1+2')
+        hAssertEqual(self, '-1', '1-2')
+        hAssertEqual(self, '2', '1*2')
+        hAssertEqual(self, '0.5', '1/2')
+        hAssertEqual(self, '8', '2 ^ 3')
 
-    def test_ex2(self):
-        h_eval('2', '2', self)
-
-    def test_basis1(self):
-        h_eval('8', '2 ^ 3', self)
-
-    def test_var1(self):
         h_parser.parser.parse('a=1')
-        h_parser.parser.parse('a')
-        check(self, '1')
+        hAssertEqual(self, '1', 'a')
 
-    def test_var2(self):
         h_parser.parser.parse('a=2')
         h_parser.parser.parse('b=3')
-        h_parser.parser.parse('a + b')
-        check(self, '5')
+        hAssertEqual(self, '5', 'a+b')
+        hAssertEqual(self, '-1', 'a-b')
+        hAssertEqual(self, '6', 'a*b')
+        hAssertEqual(self, '6', 'ab')
+        hAssertEqual(self, '0.6666666666666666', 'a/b')
 
-    def test_var3(self):
-        h_parser.parser.parse('a=2')
-        h_parser.parser.parse('b=3')
-        h_parser.parser.parse('a - b')
-        check(self, '-1')
-
-    def test_var4(self):
-        h_parser.parser.parse('a=2')
-        h_parser.parser.parse('b=3')
-        h_parser.parser.parse('a * b')
-        check(self, '6')
-
-    def test_var5(self):
-        h_parser.parser.parse('a=2')
-        h_parser.parser.parse('b=3')
-        h_parser.parser.parse('a / b')
-        check(self, '0.6666666666666666')
-
-    def test_var_multi1(self):
-        h_parser.parser.parse('a=2')
-        h_parser.parser.parse('b=3')
-        h_parser.parser.parse('ab')
-        check(self, '6')
-
-    def test_var_multi2(self):
         h_parser.parser.parse('c=23')
         h_parser.parser.parse('i=34')
         h_parser.parser.parse('j=2')
-        h_parser.parser.parse('cij')
-        check(self, '1564')
+        hAssertEqual(self, '1564', 'cij')
 
-    def test_var_multi3(self):
-        h_eval('2x', '2x', self)
+        hAssertEqual(self, '2x', '2x')
+        #h_eval('(x^6)(y^3)(z^2)', 'xxxxyyyxxzz', self)
+        # hAssertEqual(self, )
+        # hAssertEqual(self, )
 
-    def test_var_multi4(self):
-        h_eval('(x^6)(y^3)(z^2)', 'xxxxyyyxxzz', self)
+    def test_func(self):
+        h_parser.parser.parse("f(x) = x * x")
+        hAssertEqual(self, '9', 'f(3)')
+        # hAssertEqual(self, 'y^2', 'f(y)')
 
-    # SYM
-    def test_sym_plus2(self):
-        h_parser.parser.parse('x+y')
-        check(self, 'x + y')
-
-    def test_sym_minus2(self):
-        h_parser.parser.parse('x-y')
-        check(self, 'x - y')
-
-    def test_sym_multi2(self):
-        h_parser.parser.parse('xy')
-        check(self, 'xy')
-
-    def test_sym_div2(self):
-        h_parser.parser.parse('x/y')
-        check(self, 'x/y')
-
-    def test_func_basis(self):
-        h_parser.parser.parse('f(x) = x * x')
-        h_parser.parser.parse('f(3)')
-        check(self, '9')
-
-    def test_func_basis1(self):
         h_parser.parser.parse('f(x,y) = xy')
-        h_parser.parser.parse('f(3,2)')
-        check(self, '6')
+        hAssertEqual(self, '6', 'f(3,2)')
 
-    def test_func_basis2(self):
-        h_parser.parser.parse('g(x,  y,  z) = xyz')
-        h_parser.parser.parse('g(3,2,3)')
-        check(self, '18')
+        h_parser.parser.parse('g(x,y,z) = xyz')
+        hAssertEqual(self, '18', 'g(3,2,3)')
 
-    def test_diff1(self):          h_eval('2x', 'd/dx(x * x)', self)
-    def test_diff2(self):          h_eval('0', 'd/dy(x * x)', self)
-    def test_diff2(self):          h_eval('2xy', 'd/dx(xxy)', self)
+    def test_diff(self):
+        hAssertEqual(self, '2x', 'd/dx(x * x)')
+        hAssertEqual(self, '0', 'd/dy(x * x)')
+        hAssertEqual(self, '2xy', 'd/dx(xxy)')
 
-    def test_inte1(self):          h_eval('x^2/2', 'S(x dx)', self)
-    def test_inte2(self):          h_eval('sin(x)', 'S(cos(x) dx)', self)
-    def test_inte3(self):          h_eval('cos(x)', 'd/dx(S(cos(x) dx))', self)
+    def test_intg(self):
+        hAssertEqual(self, 'x^2/2', 'S(x dx)')
+        hAssertEqual(self, 'sin(x)', 'S(cos(x) dx)')
+        hAssertEqual(self, 'cos(x)', 'd/dx(S(cos(x) dx))')
 
-    def test_build_in_func1(self): h_eval('cos(x)',  'cos(x)',       self)
-    def test_build_in_func2(self): h_eval('-sin(x)', 'd/dx(cos(x))', self)
-    def test_build_in_func3(self): h_eval('0',       'd/dy(cos(x))', self)
-    def test_build_in_func4(self): h_eval('cos(1)',  'cos(1)',       self)
+    def test_buildin_func(self):
+        hAssertEqual(self, 'cos(x)', 'cos(x)')
+        hAssertEqual(self, '-sin(x)', 'd/dx(cos(x))')
+        hAssertEqual(self, '0', 'd/dy(cos(x))')
+        hAssertEqual(self, 'cos(1)', 'cos(1)')
 
-    def test_constants1(self): h_eval('e',  'e',  self)
-    def test_constants2(self): h_eval('3.141592653589793', 'pi', self)
-    def test_constants3(self): h_eval('oo', 'oo', self)
-    def test_constants4(self): h_eval('e^3',  'e^3',  self)
-    def test_constants5(self): h_eval('e^x',  'e^x',  self)
-    def test_constants6(self): h_eval('e^x',  'd/dx(e^x)',  self)
+    def test_constants(self):
+        hAssertEqual(self, 'e', 'e')
+        hAssertEqual(self, '3.141592653589793', 'pi')
+        hAssertEqual(self, 'oo', 'oo')
+        hAssertEqual(self, 'e^3', 'e^3')
+        hAssertEqual(self, 'e^x', 'e^x')
+        hAssertEqual(self, 'e^x', 'd/dx(e^x)')
 
-    def test_lim1(self): h_eval('1', 'lim[x->1](x)', self)
-
-
+    def test_limit(self):
+        hAssertEqual(self, '1', 'lim[x->1](x)')
+        hAssertEqual(self, '1', 'lim[x->1](1/x)')
+        hAssertEqual(self, '0', 'lim[x->oo](1/x)')
 
     def tearDown(self):
         sys.stdout = sys.__stdout__
